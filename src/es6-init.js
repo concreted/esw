06903fa1b7de0a8f2d9f2f3f44d23d63cdae7ac2
Aric Huang <arichuang@gmail.com>
2017-04-29 11:04:13 -0700

    testing with electron-compile

---
+++ src/es6-init.js
@@ -0,0 +1,6 @@
+// Assuming this file is ./src/es6-init.js
+var path = require('path');
+
+var appRoot = path.join(__dirname, '..');
+
+require('electron-compile').init(appRoot, require.resolve('./index'));
